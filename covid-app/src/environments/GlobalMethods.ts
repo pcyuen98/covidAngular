318b15cb138e4a29ed5f1a37543196780d4674fc
unknown <pcyuen98@gmail.com>
2021-03-28 11:45:09 +0800

    updates

--- covid-app/src/environments/GlobalMethods.ts
+++ covid-app/src/environments/GlobalMethods.ts
@@ -5,33 +5,14 @@ import { GlobalConstants } from './GlobalConstants';
 export class GlobalMethods {
 
     public static getError(error: any) {
-
+        let status = error.status;
         if (error.status != undefined) {
-            let status = error.status;
-            if (status == '404') {
-                return error.message;
+
+            if (status == '0' || status == '400') {
+                return (this.getErrorStatus(error));
             }
             else if (status == '500') {
-
-
-
-                if (error.error != undefined) {
-                    if (error.error.message != undefined) {
-                        let errorMessage = error.error.message;
-                        let isCovidBE = GlobalConstants.COVID_APP;
-                        let isBEMessage = errorMessage.indexOf(isCovidBE);
-
-                        if (isBEMessage) {
-                            return errorMessage;
-                        }
-                    }
-
-                    return error.error;
-                }
-
-            }
-            else if (status == '0') {
-                return error.message;
+                return (this.getError500Status(error));
             }
         }
 
@@ -42,6 +23,29 @@ export class GlobalMethods {
             return error;
         }
 
+    }
+
+    public static getErrorStatus(error: any) {
+
+        return error.message;
+    }
+
+    public static getError500Status(error: any) {
+
+
+        if (error.error != undefined) {
+            if (error.error.message != undefined) {
+                let errorMessage = error.error.message;
+                let isCovidBE = GlobalConstants.COVID_APP;
+                let isBEMessage = errorMessage.indexOf(isCovidBE);
+
+                if (isBEMessage) {
+                    return errorMessage;
+                }
+            }
+
+            return error.error;
+        }
     }
 
 }
