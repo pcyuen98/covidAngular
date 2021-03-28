ce321c9c94901b2f9200bf394cbb8b96848b8a67
unknown <pcyuen98@gmail.com>
2021-03-28 10:53:34 +0800

    updates

--- covid-app/src/environments/GlobalMethods.ts
+++ covid-app/src/environments/GlobalMethods.ts
@@ -7,7 +7,32 @@ export class GlobalMethods {
     public static getError(error: any) {
 
         if (error.status != undefined) {
-           return (this.getErrorStatus(error));
+            let status = error.status;
+            if (status == '404') {
+                return error.message;
+            }
+            else if (status == '500') {
+
+
+
+                if (error.error != undefined) {
+                    if (error.error.message != undefined) {
+                        let errorMessage = error.error.message;
+                        let isCovidBE = GlobalConstants.COVID_APP;
+                        let isBEMessage = errorMessage.indexOf(isCovidBE);
+
+                        if (isBEMessage) {
+                            return errorMessage;
+                        }
+                    }
+
+                    return error.error;
+                }
+
+            }
+            else if (status == '0') {
+                return error.message;
+            }
         }
 
         else if (error.message != undefined) {
@@ -17,33 +42,6 @@ export class GlobalMethods {
             return error;
         }
 
-    }
-
-    public static getErrorStatus(error: any) {
-        let status = error.status;
-        if (status == '404') {
-            return error.message;
-        }
-        else if (status == '500') {
-
-            if (error.error != undefined) {
-                if (error.error.message != undefined) {
-                    let errorMessage = error.error.message;
-                    let isCovidBE = GlobalConstants.COVID_APP;
-                    let isBEMessage = errorMessage.indexOf(isCovidBE);
-
-                    if (isBEMessage) {
-                        return errorMessage;
-                    }
-                }
-
-                return error.error;
-            }
-
-        }
-        else if (status == '0') {
-            return error.message;
-        }
     }
 
 }
