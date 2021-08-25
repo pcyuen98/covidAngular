7e0a384249209325b62cc299eae17b5bb7401741
User <pcyuen98@gmail.com>
2021-08-25 21:16:00 +0800

    Update

--- covid-app/src/environments/GlobalConstants.ts
+++ covid-app/src/environments/GlobalConstants.ts
@@ -1,10 +1,11 @@
 export class GlobalConstants {
 
-    public static helloApiURL: string = "http://139.99.33.99:8092";      
+    public static helloApiURL: string = "http://localhost:8081";      
 
     public static errorMessage: string = "BackEnd Error Found";
 
     public static errorMessageFE: string = "Front End Error Found";
 
     public static COVID_APP: string = "COVID-APP";
+    
 }
